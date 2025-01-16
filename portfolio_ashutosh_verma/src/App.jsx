d6fef89e053545b8900539bcb9b748f7669003ba
Ashutosh Verma <ashutosh01work@gmail.com>
2025-01-16 18:46:48 +0530

    first commit

---
+++ portfolio_ashutosh_verma/src/App.jsx
@@ -0,0 +1,9 @@
+import Navbar from "./components/navbar/Navbar";
+
+function App(){
+  return <div className="bg-[#171d32] h-auto w-full overfloe-hidden">
+      <Navbar/>
+  </div>
+}
+
+export default App;
